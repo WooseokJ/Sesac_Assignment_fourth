c4ef70214a652698c4f6eeefc5238010205426aa
Wooseok <wooseokbird.dev@gmail.com>
2022-10-12 12:21:25 +0900

    feat: 다양한 scene에 대응하는 push notification , method swizzling

--- FireBaseExample/FireBaseExample/AppDelegate.swift
+++ FireBaseExample/FireBaseExample/AppDelegate.swift
@@ -17,7 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
+        FirebaseApp.configure() //파이어베이스에 xcode연결
+        
+        // 원격 알림 시스템에 앱 등록(꼭 didFinishLaunchingWithOptions에 적기)
         if #available(iOS 10.0, *) {
           // For iOS 10 display notification (sent via APNS)
           UNUserNotificationCenter.current().delegate = self
@@ -32,16 +34,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
           application.registerUserNotificationSettings(settings)
         }
         application.registerForRemoteNotifications()
-        // 메시지 대리자 설정
+        
+        // 메시지 대리자 설정(꼭 didFinishLaunchingWithOptions에 적기)
         Messaging.messaging().delegate = self
-        // 현재 등록된 디바이스 토큰 가져오기
-        Messaging.messaging().token { token, error in
-          if let error = error {
-            print("Error fetching FCM registration token: \(error)")
-          } else if let token = token {
-            print("FCM registration token: \(token)")
-          }
-        }
+        
+        // 현재 등록된 디바이스 토큰 가져오기(위치는 다른곳도 가능)
+//        Messaging.messaging().token { token, error in
+//          if let error = error {
+//            print("Error fetching FCM registration token: \(error)")
+//          } else if let token = token {
+//            print("FCM registration token: \(token)")
+//          }
+//        }
+        
+        UIViewController.swizzleMethod()
+        // UIViewcontroller().swizzleMethod 는?
         return true
     }
 
@@ -65,31 +72,51 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
     
     // forground일떄 알림오게 해주는 메소드(로컬/푸시 동일)
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        // .banner, .list 는 ios14 이상부터사용가능( 그이전엔 .alert 이였다)
-        completionHandler([.badge,.sound,.banner,.list])
+        // setting화면에 있다면 포그라운드 푸시 띄우지마라
+        guard let viewController = (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.window?.rootViewController?.topViewController else {return} //topviewController는 VC에서 만든거
+        
+        if viewController is SettingViewController { // Setting 화면일떄 알림이 안옴.
+            completionHandler([]) //알림이 안옴.
+        } else { // Setting 화면이 아닐떄 알림이온다.
+            // .banner, .list 는 ios14 이상부터사용가능( 그이전엔 .alert 이였다)
+            completionHandler([.badge,.sound,.banner,.list])
+        }
+
     }
     
-    // 푸쉬클릭: 쿠팡 장바구니 담는 화면 전환
-    // 유저가 푸시를 클릭시 수신확인가능
+    // 푸쉬클릭: 특정푸시 클릭하면 특정상세화면으로 화면전환 ex) 쿠팡 장바구니 담는 화면 전환
+    // 위같은 상황에서 주의할떄 쿠팡장바구니를 화면전환하기위해 그앞에 화면들도 계층적으로 같이 보여줘야한다.
+    
+    // 유저가 푸시를 클릭시에만 실행
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         print("사용자가 푸시를 클릭함.")
         print(response.notification.request.content.body)
-//        print(response.notification.request.content.userInfo)
+        print(response.notification.request.content.userInfo)
         let userInfo = response.notification.request.content.userInfo
+        
+        // 앱에서 사용할떄 ex) 가방광고알림이면 가방광고 상세페이지로 이동 시키고 , 화장품광고이면 화장품광고 상세페이지로 이동시킬떄 쓴다.
         if userInfo[AnyHashable("sesac")] as? String == "project" { //sesac이라는 키에 project라는 값이있다면?
             print("sesac project ===================================================================")
         } else {
             print("nothing==========================================================================")
         }
         
+        guard let viewController = (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.window?.rootViewController?.topViewController else {return} //topviewController는 VC에서 만든거 
+        print(viewController)
         
+        if viewController is ViewController { // is as , is: 인스턴스와 클래스가 같은지 판단할떄 사용, 홈화면일떄 setingViewController로 push된다.
+            viewController.navigationController?.pushViewController(SettingViewController(), animated: true)
+        } else if viewController is ProfileViewControler { // present로 올라와있기떄매 내려줌.
+            viewController.dismiss(animated: true) {
+                print("present 내려감.")
+            }
+        }
     }
-    
-    
 }
 extension AppDelegate: MessagingDelegate {
     // 토큰갱신 모니터링 (선택사항) : 토큰정보가 언제바뀔까?
     // 이걸안하면 ex) 앱은 설치되어있는데 회원탈퇴해도 알림이 계속온다
+    // 현재 등록 토큰 가져오기
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
       print("Firebase registration token: \(String(describing: fcmToken))")
       let dataDict: [String: String] = ["token": fcmToken ?? ""]
--- FireBaseExample/FireBaseExample/ViewController.swift
+++ FireBaseExample/FireBaseExample/ViewController.swift
@@ -1,37 +1,107 @@
-//
-//  ViewController.swift
-//  FireBaseExample
-//
-//  Created by useok on 2022/10/05.
-//
 
 import UIKit
 import FirebaseAnalytics
 
 class ViewController: UIViewController {
-
-
-
-
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        print("viewcontroller viewwillAppear")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Analytics.logEvent("share_image", parameters: [
-            "name": "고래밥", // userdefault or realm 에서 가져와 써도되고 String값을 지정해도된다
-          "full_text": "안녕",
-        ])
-        
-        Analytics.setDefaultEventParameters([
-          "level_name": "Caverns01",
-          "level_difficulty": 4
-        ])
-        
+//        Analytics.logEvent("share_image", parameters: [
+//            "name": "고래밥", // userdefault or realm 에서 가져와 써도되고 String값을 지정해도된다
+//          "full_text": "안녕",
+//        ])
+//
+//        Analytics.setDefaultEventParameters([
+//          "level_name": "Caverns01",
+//          "level_difficulty": 4
+//        ])
     }
     
-    @IBAction func buttonClicked(_ sender: Any) {
+    @IBAction func buttonClicked(_ sender: UIButton) {}
+    //present
+    @IBAction func profileButtonClicked(_ sender: UIButton) {
+        present(ProfileViewControler(),animated: true)
+    }
+    //push
+    @IBAction func settingButtonClicked(_ sender: UIButton) {
+        navigationController?.pushViewController(SettingViewController(), animated: true)
     }
     
-
+    
 }
 
+
+class ProfileViewControler: UIViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        print("profileviewcontroller viewwillAppear")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+    
+    
+}
+
+class SettingViewController: UIViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        print("settingviewcontroller viewwillAppear")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+    
+}
+
+
+extension UIViewController {
+    var topViewController: UIViewController? { // get
+        return self.topViewController(currentVC: self)
+    }
+    
+    // 최상위 viewcontroller를 판단해주는 메서드
+    func topViewController(currentVC: UIViewController) -> UIViewController {
+        if let tabBarController = currentVC as? UITabBarController, let selectedVC = tabBarController.selectedViewController { //tabbar붙어있는경우
+            return self.topViewController(currentVC: selectedVC)
+        }
+        else if let navigationController = currentVC as? UINavigationController, let visibleVC = navigationController.visibleViewController { //네비게이션 붙어있는경우
+            return self.topViewController(currentVC: visibleVC)
+        }
+        else if let presentedViewController = currentVC.presentedViewController { // 프레젠트 붙어있는경우
+            return self.topViewController(currentVC: presentedViewController)
+        }
+        else {
+            return currentVC
+        }
+    }
+}
+
+extension UIViewController {
+    // method swizzling , 모든 viewwillAppear를 chageviewWillAppear로 바꿔준다.
+    class func swizzleMethod() {  // class와 static 의 차이는? 
+        let origin = #selector(viewWillAppear) // 원래 메소드
+        let chagned = #selector(changeViewWillAppear) // 바꾼 메소드
+        
+        guard let originMethod = class_getInstanceMethod(UIViewController.self, origin) ,
+              let chageMethod = class_getInstanceMethod(UIViewController.self, chagned) else {
+            print("함수 찾을수없거나 오류")
+            return
+        }
+        method_exchangeImplementations(originMethod, chageMethod)
+        
+    }
+        
+    @objc func changeViewWillAppear() {
+        print("change viewwillAppear succeed")
+    }
+    
+    
+    
+}
